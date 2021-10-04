27879854178367e0dbc02b90a9c4ccc5f4e158f1
Indro <indrohoffmann@tutamail.com>
2021-10-04 10:44:06 +0300

    tegin tagasi kõik, ei saanud toimima

--- javascriptkailin.js
+++ javascriptkailin.js
@@ -14,6 +14,24 @@ let contentSanat = document.getElementById("content-sanat");
 let contentContact = document.getElementById("content-contact");
 let contactConfirm = document.getElementById("contactConfirm");
 
+
+
+var menuBtn = document.getElementById('menuBtn');
+var menu = document.getElementById('menu');
+var closeMenuBtn = document.getElementById('closeMenuBtn');
+
+// Menu button click event handler
+menuBtn.onclick = function() {
+    menu.classList.add('show');
+};
+
+// Close menu button click event handler
+closeMenuBtn.onclick = function() {
+    menu.classList.remove('show');
+};
+
+
+
 contentInfo.style.display = "none";
 contentMedia.style.display = "none";
 contentSanat.style.display = "none";
